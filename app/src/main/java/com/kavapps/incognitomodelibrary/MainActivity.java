e7c75abe008082eec473a34eceaa8f20a838a400
Arthur <kavappsmail@gmail.com>
2021-01-15 09:14:06 +0200

    second commit

---
+++ app/src/main/java/com/kavapps/incognitomodelibrary/MainActivity.java
@@ -0,0 +1,18 @@
+package com.kavapps.incognitomodelibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.kavapps.library.Incognito;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
